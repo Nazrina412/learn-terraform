140906aeeb54a6142b2338536067e516021427a0
Nazrina <nazrinakhan88@gmail.com>
2024-07-15 16:00:00 -0400

    ansible variables

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
