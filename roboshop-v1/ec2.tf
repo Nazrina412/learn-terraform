f0883abfd8960b26ab974c254fda05068ca76151
Nazrina <nazrinakhan88@gmail.com>
2024-07-15 16:16:21 -0400

    ansible variables

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "frontend" {
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
 
   tags = {
-    Name = "frontend"
+    Name = "frontend.dev"
   }
 }
 
@@ -14,7 +14,7 @@ resource "aws_instance" "mongo" {
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
 
   tags = {
-    Name = "mongo"
+    Name = "mongo.dev"
   }
 }
 
@@ -24,6 +24,6 @@ resource "aws_instance" "catalogue" {
   vpc_security_group_ids = ["sg-0a0ad57f3e45d4556"]
 
   tags = {
-    Name = "catalogue"
+    Name = "catalogue.dev"
   }
 }
