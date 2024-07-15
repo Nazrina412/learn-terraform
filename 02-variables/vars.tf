3284e42ed4544f534b39bfa60c7d408e5f360332
Nazrina <nazrinakhan88@gmail.com>
2024-07-15 16:56:37 -0400

    ansible variables

--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -1,10 +1,33 @@
+
+# plain variable
+#Number
 variable "x" {
     default = 10
 }
 
+#Boolean
+variable "y" {
+    default = true
+}
+
+#string
+varibale "z" {
+    default = "Hello"
+}
+
+
+# values numbers and booleans need not to be quoted
 
 
 #outputs
+#direct valude does not required to be accessed with ${}
+
 output "x" {
     value = var.x
+}
+
+#accessing variables has a combination with a string we need to refer variable with $
+output "x1" {
+    value = "value of x - ${var.x}"
+
 }
