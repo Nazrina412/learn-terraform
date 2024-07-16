2b964b8d190865110102c5b709bea3fa5a0d65c5
Nazrina <nazrinakhan88@gmail.com>
2024-07-16 19:55:57 -0400

    ansible variables

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "frontend" {
-  ami           = "var.ami"
-  instance_type = "var.instance_type"
+  ami           = var.ami
+  instance_type = var.instance_type
   vpc_security_group_ids = var.security_group_ids
 
   tags = {
@@ -11,15 +11,15 @@ resource "aws_instance" "frontend" {
 
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "var.zone_id"
+  zone_id = var.zone_id
   name    = "frontend.dev.$(var.domain_name)"
   type    = "A"
   ttl     = 15
   records = [aws_instance.frontend.private_ip]
 }
 resource "aws_instance" "mongo" {
-  ami           = "var.ami"
-    instance_type = "var.instance_type"
+  ami           = var.ami
+    instance_type = var.instance_type
     vpc_security_group_ids = var.security_group_ids
 
   tags = {
@@ -28,7 +28,7 @@ resource "aws_instance" "mongo" {
 }
 
 resource "aws_route53_record" "mongo" {
-  zone_id = "var.zone_id"
+  zone_id = var.zone_id
   name    = "mongo.dev.(var.domain_name)"
   type    = "A"
   ttl     = 15
@@ -36,8 +36,8 @@ resource "aws_route53_record" "mongo" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = "var.ami"
-    instance_type = "var.instance_type"
+  ami           = var.ami
+    instance_type = var.instance_type
     vpc_security_group_ids = var.security_group_ids
 
   tags = {
@@ -46,7 +46,7 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = "var.zone_id"
+  zone_id = var.zone_id
   name    = "catalogue.dev.(var.domain_name)"
   type    = "A"
   ttl     = 15
