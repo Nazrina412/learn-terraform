841a8ef4d588cba46be7cc887b445a8b4abfe4e3
Nazrina <nazrinakhan88@gmail.com>
2024-07-16 22:33:12 -0400

    ansible variables

--- 01-loops/main.tf
+++ 01-loops/main.tf
@@ -1,3 +1,12 @@
 resource "null_resource" "test" {
     count = 3
 }
+
+variable "component" {
+    default = ["frontend", "catalogue", "mongo"]
+
+}
+
+resource "null_resource" "test" {
+    count = length(var.components)
+}
