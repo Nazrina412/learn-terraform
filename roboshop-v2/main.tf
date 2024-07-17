87ee193e0524a91a4e450041f1f9dd4c6ab46f98
Nazrina <nazrinakhan88@gmail.com>
2024-07-17 19:14:22 -0400

    ansible variables

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -11,4 +11,12 @@ resource "aws_instance" "instance" {
 }
 
 
+resource "aws_route53_record" "dns_record" {
+    count = length(var.components)
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.components[count.index]}.dev."
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance.private_ip[count.index]]
+}
 
