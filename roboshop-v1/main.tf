58ab4e71f45733dd8a6c8494facdb5d9b31db29d
Nazrina <nazrinakhan88@gmail.com>
2024-07-16 22:01:51 -0400

    ansible variables

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "frontend" {
-  ami           = var.ami
+  ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group_ids
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "frontend.dev"
@@ -11,16 +11,16 @@ resource "aws_instance" "frontend" {
 
 
 resource "aws_route53_record" "frontend" {
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.zone.zone.id
   name    = "frontend.dev.$(var.domain_name)"
   type    = "A"
   ttl     = 15
   records = [aws_instance.frontend.private_ip]
 }
 resource "aws_instance" "mongo" {
-  ami           = var.ami
+  ami           = data.aws_ami.ami.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = var.security_group_ids
+    vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "mongo.dev"
@@ -28,7 +28,7 @@ resource "aws_instance" "mongo" {
 }
 
 resource "aws_route53_record" "mongo" {
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.zone.zone.id
   name    = "mongo.dev.(var.domain_name)"
   type    = "A"
   ttl     = 15
@@ -36,9 +36,9 @@ resource "aws_route53_record" "mongo" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = var.ami
+  ami           = data.aws_ami.ami.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = var.security_group_ids
+    vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "catalogue.dev"
@@ -46,7 +46,7 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.zone.zone.id
   name    = "catalogue.dev.(var.domain_name)"
   type    = "A"
   ttl     = 15
--- roboshop-v1/variable.tf
+++ roboshop-v1/variable.tf
@@ -1,15 +1,12 @@
-variable "ami" {
-    default = "ami-041e2ea9402c46c32"
 
-}
+
+
 
 variable "instance_type" {
     default = "t3.small"
 }
 
-variable "zone_id" {
-    default = "Z03416193I6UORBOGZ0Y"
-}
+
 
 
 variable "domain_name" {
@@ -17,6 +14,3 @@ variable "domain_name" {
 
 }
 
-variable "security_group_ids" {
-    default = ["sg-0a0ad57f3e45d4556"]
-}
