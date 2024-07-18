dfad98d7f8f9f8f113c5df2568b75b9775842c03
Nazrina <nazrinakhan88@gmail.com>
2024-07-18 19:09:30 -0400

    ansible variables

--- 03-modules/ec2/main.tf
+++ 03-modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-041e2ea9402c46c32"
     instance_type = "t3.micro"
-    vpc_security_group_ids = "sg-0a0ad57f3e45d4556"
+    vpc_security_group_ids = sg-0a0ad57f3e45d4556
 
   tags = {
     Name = "test"
