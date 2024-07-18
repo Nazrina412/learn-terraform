5b0cc6d70f577e29f8e395ce6b0d136cdf0c9ab6
Nazrina <nazrinakhan88@gmail.com>
2024-07-17 21:13:13 -0400

    ansible variables

--- 01-conditions/main.tf
+++ 01-conditions/main.tf
@@ -1,10 +1,10 @@
 ##conditions in terraform is to pick a value, not about running the bl
-
+## expression ? True_value : False_value
 resource "aws_route53_record" "frontend" {
   zone_id = "Z00376861T6KFA01SJSIS"
   name    = "test.naifah.online"
   type    = "A"
-  ttl     = var.ttl
+  ttl     = var.ttl == "" ? 20 : var.ttl
   records = ["1.1.1.1"]
 }
 
