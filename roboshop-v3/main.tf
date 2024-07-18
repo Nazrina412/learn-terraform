8b71f1e37d92bf375120e3d83e80a273746856f4
Nazrina <nazrinakhan88@gmail.com>
2024-07-17 20:06:27 -0400

    ansible variables

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -12,7 +12,8 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "dns_record" {
-    for_each = var.components
+
+  for_each = var.components
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "$(each.key).dev.$(var.domain_name)"
   type    = "A"
